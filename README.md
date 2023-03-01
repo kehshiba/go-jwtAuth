8b474223c2636b433b1fcc6e561a561ec0fd9670
Abhishek A.B <57587313+kehshiba@users.noreply.github.com>
2023-03-01 16:29:30 +0530

    initial

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# JWT Authorization in GoLang
+
+Simple template for JWT Auth in Go.
+`Depreceated!`
+
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/kehshiba/go-jwtAuth
+```
+
+Go to the project directory
+
+```bash
+  cd go-jwtAuth
+```
+
+Start the server
+
+```
+   go run main.go controllers.go
+```
+
+
+## To update
+
+- Refactor in GoFiber 
---
+++ controllers.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"net/http"
+	"time"
+)
+
+var jwtKey = []byte("secret") //secret key
+
+var users = map[string]string{
+	"user": "password", // connect with db instead of map for production
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if expectedPass, ok := users[user.Username]; !ok || expectedPass != user.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	expTime := time.Now().Add(time.Minute * 5)
+	claims := &Claims{
+		Username: user.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w,
+		&http.Cookie{Name: "token",
+			Value:   tokenString,
+			Expires: expTime})
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tokenStr := cookie.Value
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Authorized %s", claims.Username)))
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	expTime := time.Now().Add(time.Minute * 5)
+
+	claims.ExpiresAt = expTime.Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:    "refresh_token",
+			Value:   tokenString,
+			Expires: expTime,
+		})
+
+}
